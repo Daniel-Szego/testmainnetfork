7460a8452e9e723dd96f44411410399e7339477f
dsz <szego.daniel@gmail.com>
2020-08-24 17:23:06 +0200

    out of gas limit at uniswap

--- index.test.js
+++ index.test.js
@@ -22,10 +22,11 @@ const startChain = async () => {
 
 const erc20 = require("@studydefi/money-legos/erc20")
 
+jest.setTimeout(100000)
+const uniswap = require("@studydefi/money-legos/uniswap")
+
 describe("do some tests", () => {
   let wallet
-
-  jest.setTimeout(30000);
 
   beforeAll(async () => {
     wallet = await startChain()
@@ -47,5 +48,46 @@ describe("do some tests", () => {
     const ethBalance = ethers.utils.formatEther(ethBalanceWei)
     expect(parseFloat(ethBalance)).toBe(1000)
   })
-})
 
+  test("buy DAI from Uniswap", async () => {
+    // 1. instantiate contracts
+    const daiContract = new ethers.Contract(
+      erc20.dai.address,
+      erc20.dai.abi,
+      wallet
+    )
+    const uniswapFactoryContract = new ethers.Contract(
+      uniswap.factory.address,
+      uniswap.factory.abi,
+      wallet
+    )
+    const daiExchangeAddress = await uniswapFactoryContract.getExchange(
+      erc20.dai.address
+    )
+    const daiExchangeContract = new ethers.Contract(
+      daiExchangeAddress,
+      uniswap.exchange.abi,
+      wallet
+    )
+  
+    // 2. do the actual swapping
+    await daiExchangeContract.ethToTokenSwapInput(
+      1, // min amount of token retrieved
+      2525644800, // random timestamp in the future (year 2050)
+      {
+        gasLimit: 8000000,
+        value: ethers.utils.parseEther("5"),
+      }
+    )
+  
+    // util function
+    const fromWei = x => ethers.utils.formatUnits(x, 18)
+  
+    // 3. check DAI balance
+    const daiBalanceWei = await daiContract.balanceOf(wallet.address)
+    const daiBalance = parseFloat(fromWei(daiBalanceWei))
+    expect(daiBalance).toBeGreaterThan(0)
+    console.log("daiBalance", daiBalance)
+  })
+
+})
