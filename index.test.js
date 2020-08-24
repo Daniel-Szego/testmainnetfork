5c2a970ec576feb871a8f9a778fa60ae3693277b
dsz <szego.daniel@gmail.com>
2020-08-24 17:01:42 +0200

    working basic

--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
 const { ethers } = require("ethers")
 const Ganache = require("ganache-core")
-var privates = require('./privates');
-
+const privates = require('./privates')
 
 const startChain = async () => {
   const ganache = Ganache.provider({
@@ -16,7 +15,37 @@ const startChain = async () => {
   })
 
   const provider = new ethers.providers.Web3Provider(ganache)
-  const wallet = new ethers.Wallet(PRIV_KEY, provider)
+  const wallet = new ethers.Wallet(privates.PRIV_KEY, provider)
 
   return wallet
 }
+
+const erc20 = require("@studydefi/money-legos/erc20")
+
+describe("do some tests", () => {
+  let wallet
+
+  jest.setTimeout(30000);
+
+  beforeAll(async () => {
+    wallet = await startChain()
+  })
+
+  test("initial DAI balance of 0", async () => {
+    const daiContract = new ethers.Contract(
+      erc20.dai.address,
+      erc20.dai.abi,
+      wallet
+    )
+    const daiBalanceWei = await daiContract.balanceOf(wallet.address)
+    const daiBalance = ethers.utils.formatUnits(daiBalanceWei, 18)
+    expect(parseFloat(daiBalance)).toBe(0)
+  })
+
+  test("initial ETH balance of 1000 ETH", async () => {
+    const ethBalanceWei = await wallet.getBalance()
+    const ethBalance = ethers.utils.formatEther(ethBalanceWei)
+    expect(parseFloat(ethBalance)).toBe(1000)
+  })
+})
+
--- privates.js
+++ privates.js
@@ -1,5 +1,5 @@
-const MAINNET_NODE_URL = "<insert mainnet node url here>"
-const PRIV_KEY = "<insert ANY private key here>"
+const MAINNET_NODE_URL = "https://mainnet.infura.io/v3/b12e6302e01b4c54a3d8755dbaa80107"
+const PRIV_KEY = "0xe6e1abb8451a4d7ce83ceff268b4eda9bd840438adb29bee586ef53d3bc32828"
 
 module.exports = {
     MAINNET_NODE_URL,
